5fc332211f1c2186a1c11b75e9f147aa34aabef0
Shantaramm <barbashina-2016@mail.ru>
2024-08-24 21:13:23 +0300

    Deploy Swagger UI to GitHub Pages

---
+++ swagger-initializer.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://gist.githubusercontent.com/Shantaramm/020a7c5214a830d8308e6db1d1d3ad32/raw/kredit.yaml",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
