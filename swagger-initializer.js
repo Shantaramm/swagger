b96bfe07ca1aa0df5f32dbf271f3df6aa111ad6f
Shantaramm <56877750+Shantaramm@users.noreply.github.com>
2024-08-28 14:20:47 +0300

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://gist.githubusercontent.com/Shantaram/020a7c5214a830d8308e6db1d1d3ad32/raw/kredit.yaml",
+    url: "https://gist.githubusercontent.com/Shantaramm/020a7c5214a830d8308e6db1d1d3ad32/raw/kredit.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
